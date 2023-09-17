0fec5141366ec555bb88db216baf8cc735452ca3
Majestic Fire <jjpwong@connect.ust.hk>
2023-09-17 16:30:16 +0800

    This is an initial commit for Comp3111LEx Project

---
+++ src/main/java/Lab1/mainApp1.java
@@ -0,0 +1,10 @@
+package Lab1;
+
+public class mainApp1 {
+    public static void main(String[] args) {
+        int b = 2;
+        int n = 11;
+        System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
+        System.out.println(n+"! = "+ myLibrary.factorial(n));
+    }
+}
