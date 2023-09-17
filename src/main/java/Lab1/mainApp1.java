bbdd976eb3e6ce9cd368c6ab2ef6c021b1f471ba
Majestic Fire <jjpwong@connect.ust.hk>
2023-09-17 16:33:05 +0800

    Add 3rd print line

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -8,5 +8,6 @@ public class mainApp1 {
         System.out.println("Program started ...");
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = "+ myLibrary.factorial(n));
+        System.out.println("Program ..Ended ...");
     }
 }
