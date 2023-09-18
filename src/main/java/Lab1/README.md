f95b39affc4e91761ab1ea0e1fea37360d9b7472
Majestic Fire <jjpwong@connect.ust.hk>
2023-09-18 09:59:33 +0800

    Add Lab2a

--- src/main/java/Lab1/README.md
+++ src/main/java/Lab1/README.md
@@ -1,3 +1,4 @@
 # Comp3111LEx
 
+Here's the screenshot of my 1st lab project in intelliJ:
 ![Lab1_Screenshot](https://github.com/Majestic-Fire/Comp3111LEx/assets/113261194/9300a8ee-a5b6-4c3a-9feb-2a1ffab91fda)
---
+++ src/main/java/Lab2a/mainApp2a.java
@@ -0,0 +1,17 @@
+package Lab2a;
+/* Comp3111LEx\Lab2a\mainApp2a.java
+main Application for Lab2 Exercise 1 */
+public class mainApp2a {
+    public static void main(String[] args) {
+        Score k = new Score();
+        k.setQuiz(90);
+        k.setMidTExam(70);
+        k.setFinalExam(85);
+        System.out.println("Quiz : "+k.Quiz);
+        System.out.println("Midterm Exam: "+k.MidTExam);
+        System.out.println("Final Exam : "+k.FinalExam);
+        System.out.println("Total Score : "+k.getScore());
+        System.out.println("Letter Grade: "+k.getGrade());
+        System.out.println("Comment : "+k.getComment());
+    }
+}
